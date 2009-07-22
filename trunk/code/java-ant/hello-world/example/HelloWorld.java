f7f9c1faffbc7c041d69a30ec62bdf2479e74d3e
daan.v.berkel.1980 <daan.v.berkel.1980@71257376-75ed-11de-b058-93adefbabda0>
2009-07-22 14:46:08 +0000

    Updated source to use more "sophisticated" java.

--- trunk/code/java-ant/hello-world/example/HelloWorld.java
+++ trunk/code/java-ant/hello-world/example/HelloWorld.java
@@ -4,6 +4,15 @@ public class HelloWorld {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World");
+		HelloWorld object = new HelloWorld();
+		
+		System.out.println(object.greeting());
+	}
+	
+	private String message = "Hello World";
+	
+	public String greeting() {
+	
+		return message;
 	}
 }
