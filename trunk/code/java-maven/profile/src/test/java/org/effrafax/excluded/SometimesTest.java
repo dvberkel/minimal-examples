cd8ba0472f361c2e00b5d1641a552ab3ce16fd02
daan.v.berkel.1980 <daan.v.berkel.1980@71257376-75ed-11de-b058-93adefbabda0>
2010-03-10 08:13:45 +0000

    Added some tests
    These tests get executed all the time.

---
+++ trunk/code/java-maven/profile/src/test/java/org/effrafax/excluded/SometimesTest.java
@@ -0,0 +1,14 @@
+package org.effrafax.excluded;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class SometimesTest
+{
+	@Test
+	public void testSometimes()
+	{
+		assertTrue(true);
+	}
+}
---
+++ trunk/code/java-maven/profile/src/test/java/org/effrafax/profile/AlwaysTest.java
@@ -0,0 +1,14 @@
+package org.effrafax.profile;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AlwaysTest
+{
+	@Test
+	public void testAlwaysExecuted()
+	{
+		assertTrue(true);
+	}
+}
