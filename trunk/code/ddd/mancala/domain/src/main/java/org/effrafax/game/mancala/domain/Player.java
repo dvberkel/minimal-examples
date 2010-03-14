caef344246de9cdec06030d8f4cd812547b7a178
daan.v.berkel.1980 <daan.v.berkel.1980@71257376-75ed-11de-b058-93adefbabda0>
2010-03-14 18:08:12 +0000

    Removed unnecessary comments

--- trunk/code/ddd/mancala/domain/src/main/java/org/effrafax/game/mancala/domain/Player.java
+++ trunk/code/ddd/mancala/domain/src/main/java/org/effrafax/game/mancala/domain/Player.java
@@ -10,31 +10,16 @@ package org.effrafax.game.mancala.domain;
  */
 public enum Player {
 	white {
-		/*
-		 * (non-Javadoc)
-		 * 
-		 * @see org.effrafax.game.mancala.domain.Player#opponent()
-		 */
 		@Override
 		public Player opponent() {
 			return Player.black;
 		}
 	},
 	black {
-		/*
-		 * (non-Javadoc)
-		 * 
-		 * @see org.effrafax.game.mancala.domain.Player#opponent()
-		 */
 		@Override
 		public Player opponent() {
 			return Player.white;
 		}
 	};
-	/**
-	 * Returns the opponent {@code Player} for this {@code Player}.
-	 * 
-	 * @return The opponent.
-	 */
 	abstract public Player opponent();
 }
