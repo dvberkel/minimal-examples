10da5a17896f0b0395c419ee1064a1a8e100b43b
daan.v.berkel.1980 <daan.v.berkel.1980@71257376-75ed-11de-b058-93adefbabda0>
2010-03-14 18:27:16 +0000

    Created factory interface and implementation

---
+++ trunk/code/ddd/mancala/domain/src/main/java/org/effrafax/game/mancala/factory/Factory.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+package org.effrafax.game.mancala.factory;
+
+/**
+ * @author dvberkel
+ */
+public interface Factory<T> {
+	public T getInstance();
+}
---
+++ trunk/code/ddd/mancala/domain/src/main/java/org/effrafax/game/mancala/factory/implementation/AbstractFactory.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package org.effrafax.game.mancala.factory.implementation;
+
+import java.util.ServiceLoader;
+
+/**
+ * @author dvberkel
+ */
+public abstract class AbstractFactory {
+	/**
+	 * Returns a specific provider of a service.
+	 * 
+	 * @param <S>
+	 *            The generic class of the provider.
+	 * @param service
+	 *            The service.
+	 * @return A provider for {@code service}.
+	 * @see java.util.ServiceLoader<S>
+	 */
+	protected <S> S getInstance(Class<S> service) {
+		ServiceLoader<S> serviceLoader = ServiceLoader.load(service);
+		for (S provider : serviceLoader) {
+			/* We are expecting only one provider */
+			return provider;
+		}
+		throw new IllegalStateException("No provider registered for service "
+				+ service.toString());
+	}
+}
