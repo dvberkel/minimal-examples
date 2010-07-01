e45163fd880061bd5b0ad35820ac479f4ea99269
daan.v.berkel.1980 <daan.v.berkel.1980@71257376-75ed-11de-b058-93adefbabda0>
2010-07-01 20:35:41 +0000

    Added an archetype
    A basic archetype is added.

---
+++ code/maven-archetype/basic-archetype/src/main/resources/archetype-resources/src/main/java/org/effrafax/Runner.java
@@ -0,0 +1,7 @@
+package org.effrafax;
+
+public class Runner {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
---
+++ code/maven-archetype/basic-archetype/src/main/resources/archetype-resources/src/test/java/org/effrafax/RunnerTest.java
@@ -0,0 +1,12 @@
+package org.effrafax;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class RunnerTest {
+	@Test
+	public void testATestAnnotation() {
+		assertTrue(true);
+	}
+}
