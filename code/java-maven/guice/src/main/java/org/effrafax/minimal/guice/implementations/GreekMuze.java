4b64d424b49df216bcd31851a6653c25ac77e230
daan.v.berkel.1980 <daan.v.berkel.1980@71257376-75ed-11de-b058-93adefbabda0>
2010-09-22 14:06:25 +0000

    Runner without guice
    I created a real example without using guice. No I will try to incorporate guice.

---
+++ code/java-maven/guice/src/main/java/org/effrafax/minimal/guice/implementations/GreekMuze.java
@@ -0,0 +1,14 @@
+package org.effrafax.minimal.guice.implementations;
+
+import org.effrafax.minimal.guice.interfaces.Muze;
+
+public class GreekMuze implements Muze
+{
+
+	@Override
+	public String inspire()
+	{
+		return "splendid";
+	}
+
+}
---
+++ code/java-maven/guice/src/main/java/org/effrafax/minimal/guice/interfaces/Muze.java
@@ -0,0 +1,6 @@
+package org.effrafax.minimal.guice.interfaces;
+
+public interface Muze
+{
+	public String inspire();
+}
