d0849bd8815778bfcec97c12e8c782afaba48b81
daan.v.berkel.1980 <daan.v.berkel.1980@71257376-75ed-11de-b058-93adefbabda0>
2010-09-22 14:12:59 +0000

    Runner with guice
    Used guice to create a dependency injected artist.

--- code/java-maven/guice/src/main/java/org/effrafax/minimal/guice/implementations/Painter.java
+++ code/java-maven/guice/src/main/java/org/effrafax/minimal/guice/implementations/Painter.java
@@ -3,10 +3,13 @@ package org.effrafax.minimal.guice.implementations;
 import org.effrafax.minimal.guice.interfaces.Artist;
 import org.effrafax.minimal.guice.interfaces.Muze;
 
+import com.google.inject.Inject;
+
 public class Painter implements Artist
 {
 	private Muze muze;
 
+	@Inject
 	public Painter(Muze muze)
 	{
 		this.muze = muze;
