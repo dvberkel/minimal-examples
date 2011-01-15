9d724f725babe63024d2d0af4aa75f41dc22f2ce
daan.v.berkel.1980 <daan.v.berkel.1980@71257376-75ed-11de-b058-93adefbabda0>
2011-01-15 11:01:01 +0000

    Added First test
    In order to create a correct example we are doing the factorization kata.

---
+++ code/java-maven/unit-test/src/main/java/org/effrafax/math/Factorization.java
@@ -0,0 +1,16 @@
+package org.effrafax.math;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Factorization
+{
+
+	public static List<Integer> factor(int n)
+	{
+		ArrayList<Integer> result = new ArrayList<Integer>();
+		result.add(n);
+		return result;
+	}
+
+}
